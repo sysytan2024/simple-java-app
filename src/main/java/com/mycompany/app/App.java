51b03df4fe923ee06563a8d2081c73f80c9af04b
SY Tan <sysytan2024@gmail.com>
2024-02-21 09:25:23 +0800

    Some changes to test webhooks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("SCM polling");
+        System.out.println("Testing webhook");
     }
 
     public String getMessage() {
